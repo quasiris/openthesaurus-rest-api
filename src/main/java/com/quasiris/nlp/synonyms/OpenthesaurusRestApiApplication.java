cfe98cb74764ea1c858d9e25950be7e88fd015d6
Thomas Bärwinkel <tbl@gmx.de>
2020-12-09 12:36:54 +0100

    initial import

---
+++ src/main/java/com/quasiris/nlp/synonyms/OpenthesaurusRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.quasiris.nlp.synonyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenthesaurusRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenthesaurusRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quasiris/nlp/synonyms/OpenthesaurusRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quasiris.nlp.synonyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenthesaurusRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
